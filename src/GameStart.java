4a2afc1c7efc578f5a9b766c8f3f6148c2a20016
Dmitry Semyonov <78773604+dsnetvoi@users.noreply.github.com>
2021-12-22 13:01:56 +0900

    Update GameStart.java

--- src/GameStart.java
+++ src/GameStart.java
@@ -1,22 +1,17 @@
 import javax.swing.*;
-
+//главное окно
 public class GameStart
 {
-
     public static void main( String[] args)
     {
-      gamepanel panel = new gamepanel();
-      JFrame startFrame = new JFrame("Waves");//Изменить
-      startFrame.setResizable(false);
+      gamepanel panel = new gamepanel();//cоздадим объект панель
+      JFrame startFrame = new JFrame("Waves");// создание окна с названием
+      startFrame.setResizable(false);// запрет на изменение формы окна
       startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//константы по опред на выход при нажатии крестика
-
-      startFrame.setContentPane(panel);
-      startFrame.pack();
-      startFrame.setLocationRelativeTo(null);
-      startFrame.setVisible(true);
-      panel.start();
-
+      startFrame.setContentPane(panel);// перенос в фрейм панели с GamePanel
+      startFrame.pack();//фрейм по размерам занимал столько сколькое его компоненты
+      startFrame.setLocationRelativeTo(null);//устанавливаем позицию окна по центру
+      startFrame.setVisible(true);// окно видемо
+      panel.start();// заускаем поток панели
     }
-
-
 }
